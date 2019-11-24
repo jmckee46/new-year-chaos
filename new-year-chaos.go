6803a213289c8516fc40f1edb36a08fb407ca9be
John McKee <jmckee3@mac.com>
2019-11-23 19:05:42 -0800

    working version

--- new-year-chaos.go
+++ new-year-chaos.go
@@ -1,3 +1,40 @@
 package main
 
+import "fmt"
+
+func minimumBribes(q []int32) {
+	qLength := int32(len(q))
+	var cost int32
+	var pos int32
+	var chaotic bool
+	var temp int32
+
+	for i := qLength - 1; i >= 0; i-- {
+		pos = i + 1
+		if q[i] != pos {
+			if q[i] > pos+2 {
+				chaotic = true
+				break
+			} else if i-1 >= 0 && q[i-1] == pos {
+				temp = q[i-1]
+				q[i-1] = q[i]
+				q[i] = temp
+				cost++
+			} else if i-2 >= 0 && q[i-2] == pos {
+				temp = q[i-2]
+				q[i-2] = q[i-1]
+				q[i-1] = q[i]
+				q[i] = temp
+				cost += 2
+			}
+		}
+
+	}
+	if chaotic {
+		fmt.Println("Too chaotic")
+	} else {
+		fmt.Println(cost)
+	}
+}
+
 func main() {}
--- spec-new-year-chaos_test.go
+++ spec-new-year-chaos_test.go
@@ -2,6 +2,34 @@ package main
 
 import "testing"
 
-func Test(t *testing.T) {
+func TestMinimumBribes1(t *testing.T) {
+	q := []int32{2, 1, 5, 3, 4}
 
+	minimumBribes(q)
 }
+
+// output should be "3"
+
+func TestMinimumBribes2(t *testing.T) {
+	q := []int32{2, 5, 1, 3, 4}
+
+	minimumBribes(q)
+}
+
+// output should be "Too chaotic"
+
+func TestMinimumBribes3(t *testing.T) {
+	q := []int32{5, 1, 2, 3, 7, 8, 6, 4}
+
+	minimumBribes(q)
+}
+
+// output should be "Too chaotic"
+
+func TestMinimumBribes4(t *testing.T) {
+	q := []int32{1, 2, 5, 3, 7, 8, 6, 4}
+
+	minimumBribes(q)
+}
+
+// output should be "7"
